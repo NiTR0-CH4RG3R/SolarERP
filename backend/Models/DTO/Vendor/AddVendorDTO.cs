0a1f53bdfcba485b87c1b28434ae1a7486d3a8ca
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-15 15:53:58 +0530

    Created Vendor DTOs

---
+++ backend/Models/DTO/Vendor/AddVendorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models.DTO.Vendor {
+	public class AddVendorDTO {
+		public required String Name { get; set; }
+		public required String Address { get; set; }
+		public String? Email { get; set; }
+		public required String Phone01 { get; set; }
+		public String? Phone02 { get; set; }
+		public String? VendorRegistrationNumber { get; set; }
+		public String? Comments { get; set; }
+	}
+}
---
+++ backend/Models/DTO/Vendor/GetVendorDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace backend.Models.DTO.Vendor {
+	public class GetVendorDTO : AddVendorDTO {
+		public Int32? Id { get; set; }
+		public Int32? LastUpdatedBy { get; set; }
+		public DateTime? LastUpdatedDateTime { get; set; }
+	}
+}
