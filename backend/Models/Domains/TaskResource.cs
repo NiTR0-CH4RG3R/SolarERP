8bbc9036adc26aafb6d154549624ca7eb4c63059
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-15 15:19:08 +0530

    Domains for TaskResources and TaskStatus

---
+++ backend/Models/Domains/TaskResource.cs
@@ -0,0 +1,18 @@
+﻿namespace backend.Models.Domains
+{
+    public enum ResourceCategories
+    {
+        Image,
+        Document,
+        Other
+    }
+    public class TaskResource
+    {
+        public required Int32 TaskId { get; set; }
+        public required String URL { get; set; }
+        public required String Category { get; set; }
+        public String? Comments { get; set; }
+        public Int32? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedDateTime { get; set; }
+    }
+}
---
+++ backend/Models/Domains/TaskStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace backend.Models.Domains
+{
+    public enum StatusCategories
+    {
+        Active,
+        Onhold, 
+        WaitingFor,
+        Invalid,
+        Rejected,
+        Done
+    }
+    public class TaskStatus
+    { 
+        public Int32? Id { get; set; }
+        public required Int32 TaskId { get; set; }
+        public required String Status { get; set; }
+        public String? Comments { get; set; }
+        public Int32? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedDateTime { get; set; }
+    }
+}
