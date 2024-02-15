1612b4fe5f07b189d342655369d3da0df77072f2
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-15 21:15:18 +0530

    Services for task resources and task status

---
+++ backend.test/Services/ServiceTaskResourceTest.cs
@@ -0,0 +1,75 @@
+﻿using backend.Models.DTO.VendorItem;
+using backend.Repositories.Interfaces;
+using backend.Repositories;
+using backend.Services.Interfaces;
+using backend.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Models.Domains;
+using System.Security.Policy;
+using backend.Models.DTO.TaskReource;
+
+namespace backend.test.Services
+{
+    public class ServiceTaskResourceTest
+    {
+        private readonly IDbConnection _connection;
+        private readonly IRepositoryParticipant _repositoryParticipant;
+        private readonly IRepositorySystemUser _repositorySystemUser;
+        private readonly IRepositoryTask _repositoryTask;
+        private readonly IRepositoryTaskResource _repositoryTaskResource;
+        private readonly ILogger<ServiceCustomer> _logger;
+        private readonly IServiceTaskResource _serviceTaskResource;
+
+        public ServiceTaskResourceTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                                    .AddLogging()
+                                    .BuildServiceProvider();
+
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+
+            _logger = factory.CreateLogger<ServiceCustomer>();
+
+            _connection = new MySqlConnection("Server=localhost;Database=new_erp;Uid=root;Pwd=;Port=3306");
+            _connection.Open();
+
+            _repositoryParticipant = new RepositoryParticipant(_connection);
+            _repositorySystemUser = new RepositorySystemUser(_connection);
+            _repositoryTask = new RepositoryTask(_connection);
+            _repositoryTaskResource = new RepositoryTaskResource(_connection);
+
+            _serviceTaskResource = new ServiceTaskResource(_repositoryParticipant, _repositorySystemUser, _repositoryTask, _repositoryTaskResource, _logger);
+        }
+
+        [Fact]
+        public async void Test_CreateAsync()
+        {
+            GetTaskResourceDTO? result = null;
+
+            try
+            {
+                result = await _serviceTaskResource.CreateAsync(2, new AddTaskResourceDTO
+                {
+                    TaskId = 2, 
+                    URL = "taskresource4.jpg",
+                    Category = TaskResourceCategory.Document.ToString(),
+                    Comments = "This is a service task resource test comment"
+                }); 
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ backend.test/Services/ServiceTaskStatusTest.cs
@@ -0,0 +1,99 @@
+﻿using backend.Models.Domains;
+using backend.Models.DTO.TaskReource;
+using backend.Repositories.Interfaces;
+using backend.Repositories;
+using backend.Services.Interfaces;
+using backend.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Models.DTO.TaskStatus;
+using backend.Models.DTO.VendorItem;
+
+namespace backend.test.Services
+{
+    public class ServiceTaskStatusTest
+    {
+        private readonly IDbConnection _connection;
+        private readonly IRepositoryParticipant _repositoryParticipant;
+        private readonly IRepositorySystemUser _repositorySystemUser;
+        private readonly IRepositoryTask _repositoryTask;
+        private readonly IRepositoryTaskStatus _repositoryTaskStatus;
+        private readonly ILogger<ServiceCustomer> _logger;
+        private readonly IServiceTaskStatus _serviceTaskStatus;
+
+        public ServiceTaskStatusTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                                    .AddLogging()
+                                    .BuildServiceProvider();
+
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+
+            _logger = factory.CreateLogger<ServiceCustomer>();
+
+            _connection = new MySqlConnection("Server=localhost;Database=new_erp;Uid=root;Pwd=;Port=3306");
+            _connection.Open();
+
+            _repositoryParticipant = new RepositoryParticipant(_connection);
+            _repositorySystemUser = new RepositorySystemUser(_connection);
+            _repositoryTask = new RepositoryTask(_connection);
+            _repositoryTaskStatus = new RepositoryTaskStatus(_connection);
+
+            _serviceTaskStatus = new ServiceTaskStatus(_repositoryParticipant, _repositorySystemUser, _repositoryTask, _repositoryTaskStatus, _logger);
+        }
+
+        [Fact]
+        public async void Test_CreateAsync()
+        {
+            GetTaskStatusDTO? result = null;
+
+            try
+            {
+                result = await _serviceTaskStatus.CreateAsync(2, new AddTaskStatusDTO
+                {
+                    TaskId = 3,
+                    Status = TaskStatusCategory.Active.ToString(),
+                    Comments = "This is a task status category for test"
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void Test_UpdateAsync()
+        {
+            GetTaskStatusDTO? result = null;
+
+            try
+            {
+                result = await _serviceTaskStatus.UpdateAsync(2, 1, new AddTaskStatusDTO
+                {
+
+                    TaskId = 2,
+                    Status = TaskStatusCategory.Active.ToString(),
+                    Comments = "Comment updated ",
+                    
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            Assert.NotNull(result);
+        }
+
+    }
+}
