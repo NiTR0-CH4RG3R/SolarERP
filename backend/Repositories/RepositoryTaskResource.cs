19b97b3415de94abb477404f51af5d347b64d3d3
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-15 15:18:14 +0530

    Repositories craeted for TaskResource and TaskStatus

---
+++ backend/Repositories/RepositoryTaskResource.cs
@@ -0,0 +1,62 @@
+﻿using backend.Models.Domains;
+using backend.Repositories.Interfaces;
+using Dapper;
+using System.Data;
+
+namespace backend.Repositories
+{
+    public class RepositoryTaskResource : IRepositoryTaskResource
+    {
+        private readonly IDbConnection _connection;
+
+        public RepositoryTaskResource(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public async Task<TaskResource> CreateAsync(TaskResource taskresource)
+        {
+            String sp = "spInsertTaskResource";
+
+            var result = await _connection.QueryAsync<TaskResource>(sp, taskresource, commandType: CommandType.StoredProcedure);
+
+            if (result != null && result.Count() > 0)
+            {
+                return result.First();
+            }
+
+            throw new Exception("No task resource was created");
+        }
+
+        public async Task<bool> DeleteByTaskIdAndURLAsync(int taskId, string URL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TaskResource>> GetAllByTaskIdAndURLAsync(int taskId, string url, int offset, int count)
+        {
+            String sp = "spSelectTaskResourceByTaskIdAndURL";
+
+			var result = await _connection.QueryAsync<Models.Domains.TaskResource>(sp, new { TaskId = taskId, URL = url, offset, count }, commandType: CommandType.StoredProcedure);
+
+			if ( result == null ) {
+				throw new Exception( "No data found" );
+			}
+
+			return result;
+        }
+
+        public async Task<IEnumerable<TaskResource>> GetAllByTaskIdAsync(int taskId, int offset, int count)
+        {
+            String sp = "spSelectTaskResourcesByTaskId";
+
+            var result = await _connection.QueryAsync<Models.Domains.TaskResource>(sp, new { TaskId = taskId, offset, count }, commandType: CommandType.StoredProcedure);
+
+            if (result == null)
+            {
+                throw new Exception("No data found");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ backend/Repositories/RepositoryTaskStatus.cs
@@ -0,0 +1,76 @@
+﻿using backend.Repositories.Interfaces;
+using System.Data;
+using Dapper;
+using System.Security.Policy;
+using backend.Models.Domains;
+
+namespace backend.Repositories
+{
+    public class RepositoryTaskStatus : IRepositoryTaskStatus
+    {
+        private readonly IDbConnection _connection;
+
+        public RepositoryTaskStatus(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public async Task<Models.Domains.TaskStatus> CreateAsync(Models.Domains.TaskStatus taskstatus)
+        {
+            String sp = "spInsertTaskStatus";
+
+            var result = await _connection.QueryAsync<Models.Domains.TaskStatus>(sp, taskstatus, commandType: CommandType.StoredProcedure);
+
+            if (result != null && result.Count() > 0)
+            {
+                return result.First();
+            }
+
+            throw new Exception("No task status was created");
+        }
+
+        public async Task<bool> DeleteById(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Models.Domains.TaskStatus>> GetAllByIdAsync(int id, int offset, int count)
+        {
+            String sp = "spSelectTaskStatusById";
+
+            var result = await _connection.QueryAsync<Models.Domains.TaskStatus>(sp, new { Id = id, offset, count }, commandType: CommandType.StoredProcedure);
+
+            if (result == null)
+            {
+                throw new Exception("No data found");
+            }
+
+            return result;
+        }
+
+        public async Task<IEnumerable<Models.Domains.TaskStatus>> GetAllByTaskIdWithLimitAsync(int taskId, int offset, int count)
+        {
+            String sp = "spSelectTaskStatusByTaskIdWithLimit";
+
+            var result = await _connection.QueryAsync<Models.Domains.TaskStatus>(sp, new { TaskId = taskId, offset, count }, commandType: CommandType.StoredProcedure);
+
+            if (result == null)
+            {
+                throw new Exception("No data found");
+            }
+
+            return result;
+        }
+
+        public async Task<Models.Domains.TaskStatus> UpdateAsync(Models.Domains.TaskStatus taskstatus)
+        {
+            String sp = "spUpdateTaskStatusById";
+            var result = await _connection.QueryAsync<Models.Domains.TaskStatus>(sp, taskstatus, commandType: CommandType.StoredProcedure);
+            if (result == null)
+            {
+                throw new Exception("No task status was updated");
+            }
+
+            return result.First();
+        }
+    }
+}
