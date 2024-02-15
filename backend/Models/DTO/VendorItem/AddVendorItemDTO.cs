df930c163f1da13dc56eb5e52d9ef7f67e609c56
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-15 15:54:47 +0530

    Created Vendor Item DTOs

---
+++ backend/Models/DTO/VendorItem/AddVendorItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models.DTO.VendorItem {
+	public class AddVendorItemDTO {
+		public required Int32 VendorId { get; set; }
+		public required String ProductName { get; set; }
+		public String? ProductCode { get; set; }
+		public String? Brand { get; set; }
+		public required float Price { get; set; }
+		public String? WarrantyDuration { get; set; }
+		public Int32? Capacity { get; set; }
+		public String? Comments { get; set; }
+	}
+}
---
+++ backend/Models/DTO/VendorItem/GetVendorItemDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace backend.Models.DTO.VendorItem {
+	public class GetVendorItemDTO : AddVendorItemDTO {
+		public Int32? Id { get; set; }
+		public Int32? LastUpdatedBy { get; set; }
+		public DateTime? LastUpdatedDateTime { get; set; }
+	}
+}
