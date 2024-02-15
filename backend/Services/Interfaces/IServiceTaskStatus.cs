9d48d6ab4f2ecf8ff9b79e57f059ba8cdf792112
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-15 16:44:35 +0530

    Added interfaces for TaskStatus and TaskResource services

---
+++ backend/Services/Interfaces/IServiceTaskStatus.cs
@@ -0,0 +1,12 @@
+﻿using backend.Models.DTO.TaskStatus;
+
+namespace backend.Services.Interfaces {
+	public interface IServiceTaskStatus {
+		Task<GetTaskStatusDTO> CreateAsync(Int32 userId, AddTaskStatusDTO taskStatus);
+		Task<GetTaskStatusDTO> GetByIdAsync(Int32 id);
+		Task<List<GetTaskStatusDTO>> GetAllByTaskAsync( Int32 userId, Int32 taskId);
+		Task<GetTaskStatusDTO> UpdateAsync(Int32 userId, Int32 id, AddTaskStatusDTO taskStatus);
+		Task<Boolean> DeleteAsync(Int32 userId, Int32 id);
+
+	}
+}
