282c6dacbfb0696b1ee2669fc5ee13d7faae9005
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-15 15:18:41 +0530

    IRepositories for TaskResource and TaskStatus

---
+++ backend/Repositories/Interfaces/IRepositoryTaskResource.cs
@@ -0,0 +1,12 @@
+﻿using backend.Models.Domains;
+
+namespace backend.Repositories.Interfaces
+{
+    public interface IRepositoryTaskResource
+    {
+        Task<IEnumerable<TaskResource>> GetAllByTaskIdAsync(Int32 taskId, Int32 offset, Int32 count);
+        Task<IEnumerable<TaskResource>> GetAllByTaskIdAndURLAsync(Int32 taskId, String url, Int32 offset, Int32 count);
+        Task<TaskResource> CreateAsync(TaskResource taskresource);
+        Task<Boolean> DeleteByTaskIdAndURLAsync(Int32 taskId, String URL);
+    }
+}
---
+++ backend/Repositories/Interfaces/IRepositoryTaskStatus.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models.Domains;
+
+namespace backend.Repositories.Interfaces
+{
+    public interface IRepositoryTaskStatus
+    {
+        Task<IEnumerable<Models.Domains.TaskStatus>> GetAllByIdAsync(Int32 id, Int32 offset, Int32 count);
+        Task<IEnumerable<Models.Domains.TaskStatus>> GetAllByTaskIdWithLimitAsync(Int32 taskId, Int32 offset, Int32 count);
+        Task<Models.Domains.TaskStatus> CreateAsync(Models.Domains.TaskStatus taskstatus);
+        Task<Models.Domains.TaskStatus> UpdateAsync(Models.Domains.TaskStatus taskstatus);
+        Task<Boolean> DeleteById(Int32 taskId);
+    }
+}
