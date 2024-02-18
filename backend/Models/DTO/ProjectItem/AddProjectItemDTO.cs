2e0c00465d3cf39ac91c72b36020f284549a745c
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-18 23:08:30 +0530

    Created Domain and DTO models for ProjectItems

---
+++ backend/Models/DTO/ProjectItem/AddProjectItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models.DTO.ProjectItem {
+	public class AddProjectItemDTO {
+		public required Int32 ProjectId { get; set; }
+		public required Int32 VendorItemId { get; set; }
+		public String? ModuleNo { get; set; }
+		public String? WarrantyDuration { get; set; }
+		public String? SerialNo { get; set; }
+		public String? Comments { get; set; }
+	}
+}
---
+++ backend/Models/Domains/ProjectItem.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models.Domains {
+	public class ProjectItem {
+		public Int32? Id { get; set; }
+		public required Int32 ProjectId { get; set; }
+		public required Int32 VendorItemId { get; set; }
+		public String? ModuleNo { get; set; }
+		public String? WarrantyDuration { get; set; }
+		public String? SerialNo { get; set; }
+		public String? Comments { get; set; }
+		public Int32? LastUpdatedBy { get; set; }
+		public DateTime? LastUpdatedDateTime { get; set; }
+	}
+}
