2b6aa5adffbe9a743ccf02f70c06a9c8b2c12d2a
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2024-02-19 16:55:26 +0530

    Add ProjectTest DTOs

---
+++ backend/Models/DTO/ProjectTest/AddProjectTestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models.DTO.ProjectTest
+{
+    public class AddProjectTestDTO
+    {
+        public required string Name { get; set; }
+        public required Int32 Passed { get; set; }
+        public Int32? ConductedBy { get; set; }
+        public DateTime? ConductedDate { get; set; }
+        public string? Comments { get; set; }
+    }
+}
---
+++ backend/Models/DTO/ProjectTest/GetProjectTestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models.DTO.ProjectTest
+{
+    public class GetProjectTestDTO : AddProjectTestDTO
+    {
+        public Int32? Id { get; set; }
+        public required Int32 ProjectId { get; set; }
+        public Int32? LastUpdatedBy { get; set; }
+        public required DateTime LastUpdatedDateTime { get; set; }
+    }
+}
