aa963b1b225a7856780f40806ec6bce5e6788e29
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2024-02-19 16:53:23 +0530

    Add ProjectService DTOs

---
+++ backend/Models/DTO/ProjectService/AddProjectServiceDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models.DTO.ProjectService
+{
+    public class AddProjectServiceDTO
+    {
+        public required DateTime PlannedDate { get; set; }
+        public required string Status { get; set; }
+        public Int32? ConductedBy { get; set; }
+        public DateTime? ConductedDate { get; set; }
+        public required string Priority { get; set; }
+        public string? Description { get; set; }
+        public string? ServiceReportURL { get; set; }
+        public string? ServiceLevel { get; set; }
+    }
+}
---
+++ backend/Models/DTO/ProjectService/GetProjectServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models.DTO.ProjectService
+{
+    public class GetProjectServiceDTO : AddProjectServiceDTO
+    {
+        public Int32? Id { get; set; }
+        public required Int32 ProjectId { get; set; }
+        public Int32? LastUpdatedBy { get; set; }
+        public required DateTime LastUpdatedDateTime { get; set; }
+    }
+}
