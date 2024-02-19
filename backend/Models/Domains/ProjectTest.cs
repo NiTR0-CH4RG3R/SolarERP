83929ca4e0244e4f0f120e7042110efcf11fa477
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2024-02-19 13:56:49 +0530

    Create ProjectTest datamodel and repository interface

---
+++ backend/Models/Domains/ProjectTest.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Models.Domains
+{
+    public class ProjectTest
+    {
+        public Int32? Id { get; set; }
+        public required Int32 ProjectId { get; set; }
+        public required string Name { get; set; }
+        public required Int32 Passed { get; set; }
+        public Int32? ConductedBy { get; set; }
+        public DateTime? ConductedDate { get; set; }
+        public string? Comments { get; set; }
+        public Int32? LastUpdatedBy { get; set; }
+        public required DateTime LastUpdatedDateTime { get; set; }
+    }
+}
