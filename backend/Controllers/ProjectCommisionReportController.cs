f1b3d8becaf8a8dfd41bfa96aba4887600c0227e
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-03-12 04:28:13 +0530

    Renamed ProjectCommisionReport to ProjectConmisionReportController and same for the ProjectItem and implemented those classes.

---
+++ backend/Controllers/ProjectCommisionReportController.cs
@@ -0,0 +1,89 @@
+﻿using backend.Models.DTO.ProjectCommisionReport;
+using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers {
+	[Route( "api/[controller]" )]
+	[ApiController]
+	public class ProjectCommisionReportController : ControllerBase {
+		private readonly IServiceProjectCommisionReport _serviceProjectCommisionReport;
+		private readonly ILogger<ProjectCommisionReportController> _logger;
+
+		public ProjectCommisionReportController( IServiceProjectCommisionReport serviceProjectCommisionReport, ILogger<ProjectCommisionReportController> logger ) {
+			_serviceProjectCommisionReport = serviceProjectCommisionReport;
+			_logger = logger;
+		}
+
+		[HttpGet( "all" )]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId ) {
+			try {
+				throw new NotImplementedException();
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId, [FromQuery] Int32 projectId, [FromQuery] Int32 page, [FromQuery] Int32 pageSize ) {
+			try {
+				var result = await _serviceProjectCommisionReport.GetAllByProjectAsync( userId, projectId, page, pageSize );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpGet( "{id:int}" )]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId, [FromRoute] Int32 id ) {
+			try {
+				var result = await _serviceProjectCommisionReport.GetByIdAsync( id );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post( [FromQuery] Int32 userId, [FromBody] AddProjectCommisionReportDTO projectCommisionReport ) {
+			try {
+				var result = await _serviceProjectCommisionReport.CreateAsync( userId, projectCommisionReport );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpPut( "{id:int}" )]
+		public async Task<IActionResult> Put( [FromQuery] Int32 userId, [FromRoute] Int32 id, [FromBody] AddProjectCommisionReportDTO projectCommisionReport ) {
+			try {
+				var result = await _serviceProjectCommisionReport.UpdateAsync( userId, id, projectCommisionReport );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpDelete( "{id:int}" )]
+		public async Task<IActionResult> Delete( [FromQuery] Int32 userId, [FromRoute] Int32 id ) {
+			try {
+				var result = await _serviceProjectCommisionReport.DeleteAsync( userId, id );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+	}
+}
---
+++ backend/Controllers/ProjectItemController.cs
@@ -0,0 +1,77 @@
+﻿using backend.Models.DTO.ProjectItem;
+using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers {
+	[Route( "api/[controller]" )]
+	[ApiController]
+	public class ProjectItemController : ControllerBase {
+		private readonly IServiceProjectItem _serviceProjectItem;
+		private readonly ILogger<ProjectItemController> _logger;
+
+		public ProjectItemController( IServiceProjectItem serviceProjectItem, ILogger<ProjectItemController> logger ) {
+			_serviceProjectItem = serviceProjectItem;
+			_logger = logger;
+		}
+
+		[HttpGet( "all" )]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId ) {
+			try {
+				throw new NotImplementedException();
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post( [FromQuery] Int32 userId, [FromBody] AddProjectItemDTO projectItem ) {
+			try {
+				var result = await _serviceProjectItem.CreateAsync( userId, projectItem );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId, [FromQuery] Int32 projectId, [FromQuery] Int32 page, [FromQuery] Int32 pageSize ) {
+			try {
+				var result = await _serviceProjectItem.GetAllByProjectAsync( userId, projectId, page, pageSize );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpGet( "{id:int}" )]
+		public async Task<IActionResult> Get( [FromQuery] Int32 userId, [FromRoute] Int32 id ) {
+			try {
+				var result = await _serviceProjectItem.GetByIdAsync( id );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+
+		[HttpPut( "{id:int}" )]
+		public async Task<IActionResult> Put( [FromQuery] Int32 userId, [FromRoute] Int32 id, [FromBody] AddProjectItemDTO projectItem ) {
+			try {
+				var result = await _serviceProjectItem.UpdateAsync( userId, id, projectItem );
+				return Ok( result );
+			}
+			catch ( Exception ex ) {
+				_logger.LogError( ex, ex.Message );
+				return BadRequest( ex.Message );
+			}
+		}
+	}
+}
