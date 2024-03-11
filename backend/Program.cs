79485cfa4a6b9efd13ee2689b10381e2ecd4ef18
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-03-11 23:19:58 +0530

    Added the mapping configurations for the AutoMapper

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 
+using AutoMapper;
 using backend.Repositories;
 using backend.Repositories.Interfaces;
 using backend.Services;
@@ -24,6 +25,51 @@ namespace backend {
 					policy.WithOrigins( builder.Configuration.GetSection( "CORS" ).GetSection( "AllowedOrigins" ).Get<String[]>()  ).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
 				} );
 			} );
+
+			// Create the automapper configuration
+			var mapperConfig = new MapperConfiguration( cfg => {
+				cfg.CreateMap<Models.Domains.Participant, Models.DTO.Customer.GetCustomerDTO>();
+				cfg.CreateMap<Models.DTO.Customer.AddCustomerDTO, Models.Domains.Participant>();
+
+				cfg.CreateMap<Models.Domains.SystemUser, Models.DTO.SystemUser.GetSystemUserDTO>();
+				cfg.CreateMap<Models.DTO.SystemUser.AddSystemUserDTO, Models.Domains.SystemUser>();
+
+				cfg.CreateMap<Models.Domains.Vendor, Models.DTO.Vendor.GetVendorDTO>();
+				cfg.CreateMap<Models.DTO.Vendor.AddVendorDTO, Models.Domains.Vendor>();
+
+				cfg.CreateMap<Models.Domains.VendorItem, Models.DTO.VendorItem.GetVendorItemDTO>();
+				cfg.CreateMap<Models.DTO.VendorItem.AddVendorItemDTO, Models.Domains.VendorItem>();
+
+				cfg.CreateMap<Models.Domains.Project, Models.DTO.Project.GetProjectDTO>();
+				cfg.CreateMap<Models.DTO.Project.AddProjectDTO, Models.Domains.Project>();
+
+				cfg.CreateMap<Models.Domains.ProjectCommisionReport, Models.DTO.ProjectCommisionReport.GetProjectCommisionReportDTO>();
+				cfg.CreateMap<Models.DTO.ProjectCommisionReport.AddProjectCommisionReportDTO, Models.Domains.ProjectCommisionReport>();
+
+				cfg.CreateMap<Models.Domains.ProjectResource, Models.DTO.ProjectResource.GetProjectResourceDTO>();
+				cfg.CreateMap<Models.DTO.ProjectResource.AddProjectResourceDTO, Models.Domains.ProjectResource>();
+
+				cfg.CreateMap<Models.Domains.ProjectItem, Models.DTO.ProjectItem.GetProjectItemDTO>();
+				cfg.CreateMap<Models.DTO.ProjectItem.AddProjectItemDTO, Models.Domains.ProjectItem>();
+
+				cfg.CreateMap<Models.Domains.ProjectService, Models.DTO.ProjectService.GetProjectServiceDTO>();
+				cfg.CreateMap<Models.DTO.ProjectService.AddProjectServiceDTO, Models.Domains.ProjectService>();
+
+				cfg.CreateMap<Models.Domains.ProjectTest, Models.DTO.ProjectTest.GetProjectTestDTO>();
+				cfg.CreateMap<Models.DTO.ProjectTest.AddProjectTestDTO, Models.Domains.ProjectTest>();
+
+				cfg.CreateMap<Models.Domains.Task, Models.DTO.Task.GetTaskDTO>();
+				cfg.CreateMap<Models.DTO.Task.AddTaskDTO, Models.Domains.Task>();
+
+				cfg.CreateMap<Models.Domains.TaskStatus, Models.DTO.TaskStatus.GetTaskStatusDTO>();
+				cfg.CreateMap<Models.DTO.TaskStatus.AddTaskStatusDTO, Models.Domains.TaskStatus>();
+
+				cfg.CreateMap<Models.Domains.TaskResource, Models.DTO.TaskResource.GetTaskResourceDTO>();
+				cfg.CreateMap<Models.DTO.TaskResource.AddTaskResourceDTO, Models.Domains.TaskResource>();
+			} );
+
+			// Add the automapper
+			builder.Services.AddSingleton( mapperConfig.CreateMapper() );
 
 			// Add the database service
 			builder.Services.AddScoped<IDbConnection, MySqlConnection>( x => new MySqlConnection( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
