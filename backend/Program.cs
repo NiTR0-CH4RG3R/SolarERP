1ca5e430d66bcc53aa8cbc31dfaba4c83fe37e99
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-04-22 21:14:50 +0530

    Added authentication and authorization support

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,8 +4,11 @@ using backend.Repositories;
 using backend.Repositories.Interfaces;
 using backend.Services;
 using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Text;
 
 namespace backend {
 	public class Program {
@@ -19,57 +22,46 @@ namespace backend {
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
+			var allowedorsOrigins = builder.Configuration.GetSection( "CORS" ).GetSection( "AllowedOrigins" ).Get<String[]>();
+			if ( allowedorsOrigins == null )
+				throw new Exception( "CORS:AllowedOrigins is not set in appsettings.json" );
+
 			// Add CORS
 			builder.Services.AddCors( options => {
 				options.AddDefaultPolicy( policy => {
-					policy.WithOrigins( builder.Configuration.GetSection( "CORS" ).GetSection( "AllowedOrigins" ).Get<String[]>()  ).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+					policy.WithOrigins( allowedorsOrigins ).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
 				} );
 			} );
 
-			// Create the automapper configuration
-			var mapperConfig = new MapperConfiguration( cfg => {
-				cfg.CreateMap<Models.Domains.Participant, Models.DTO.Customer.GetCustomerDTO>();
-				cfg.CreateMap<Models.DTO.Customer.AddCustomerDTO, Models.Domains.Participant>();
+			var jwtSecret = builder.Configuration["Jwt:AccessTokenSecret"];
+			if ( jwtSecret == null )
+				throw new Exception( "Jwt:AccessTokenSecret is not set in appsettings.json" );
 
-				cfg.CreateMap<Models.Domains.SystemUser, Models.DTO.SystemUser.GetSystemUserDTO>();
-				cfg.CreateMap<Models.DTO.SystemUser.AddSystemUserDTO, Models.Domains.SystemUser>();
+			// Add the authentication service
+			builder.Services.AddAuthentication(
+				options => {
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				} ).AddJwtBearer( options => {
+					options.TokenValidationParameters = new TokenValidationParameters {
+						ValidateIssuer = false,
+						ValidateAudience = false,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey( Encoding.ASCII.GetBytes( jwtSecret ) )
+					};
+				}
+			);
 
-				cfg.CreateMap<Models.Domains.Vendor, Models.DTO.Vendor.GetVendorDTO>();
-				cfg.CreateMap<Models.DTO.Vendor.AddVendorDTO, Models.Domains.Vendor>();
-
-				cfg.CreateMap<Models.Domains.VendorItem, Models.DTO.VendorItem.GetVendorItemDTO>();
-				cfg.CreateMap<Models.DTO.VendorItem.AddVendorItemDTO, Models.Domains.VendorItem>();
-
-				cfg.CreateMap<Models.Domains.Project, Models.DTO.Project.GetProjectDTO>();
-				cfg.CreateMap<Models.DTO.Project.AddProjectDTO, Models.Domains.Project>();
-
-				cfg.CreateMap<Models.Domains.ProjectCommisionReport, Models.DTO.ProjectCommisionReport.GetProjectCommisionReportDTO>();
-				cfg.CreateMap<Models.DTO.ProjectCommisionReport.AddProjectCommisionReportDTO, Models.Domains.ProjectCommisionReport>();
-
-				cfg.CreateMap<Models.Domains.ProjectResource, Models.DTO.ProjectResource.GetProjectResourceDTO>();
-				cfg.CreateMap<Models.DTO.ProjectResource.AddProjectResourceDTO, Models.Domains.ProjectResource>();
-
-				cfg.CreateMap<Models.Domains.ProjectItem, Models.DTO.ProjectItem.GetProjectItemDTO>();
-				cfg.CreateMap<Models.DTO.ProjectItem.AddProjectItemDTO, Models.Domains.ProjectItem>();
-
-				cfg.CreateMap<Models.Domains.ProjectService, Models.DTO.ProjectService.GetProjectServiceDTO>();
-				cfg.CreateMap<Models.DTO.ProjectService.AddProjectServiceDTO, Models.Domains.ProjectService>();
-
-				cfg.CreateMap<Models.Domains.ProjectTest, Models.DTO.ProjectTest.GetProjectTestDTO>();
-				cfg.CreateMap<Models.DTO.ProjectTest.AddProjectTestDTO, Models.Domains.ProjectTest>();
-
-				cfg.CreateMap<Models.Domains.Task, Models.DTO.Task.GetTaskDTO>();
-				cfg.CreateMap<Models.DTO.Task.AddTaskDTO, Models.Domains.Task>();
-
-				cfg.CreateMap<Models.Domains.TaskStatus, Models.DTO.TaskStatus.GetTaskStatusDTO>();
-				cfg.CreateMap<Models.DTO.TaskStatus.AddTaskStatusDTO, Models.Domains.TaskStatus>();
-
-				cfg.CreateMap<Models.Domains.TaskResource, Models.DTO.TaskResource.GetTaskResourceDTO>();
-				cfg.CreateMap<Models.DTO.TaskResource.AddTaskResourceDTO, Models.Domains.TaskResource>();
-			} );
+			builder.Services.AddAuthorization(
+					option => {
+						option.AddPolicy( "Admin", policy => policy.RequireClaim( "role", "Admin" ) );
+						option.AddPolicy( "User", policy => policy.RequireClaim( "role", "User" ) );
+					}
+				);
 
 			// Add the automapper
-			builder.Services.AddSingleton( mapperConfig.CreateMapper() );
+			builder.Services.AddSingleton( MapperConfig.Configure().CreateMapper() );
 
 			// Add the database service
 			builder.Services.AddScoped<IDbConnection, MySqlConnection>( x => new MySqlConnection( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
@@ -89,6 +81,7 @@ namespace backend {
 			builder.Services.AddScoped<IRepositoryProjectTest, RepositoryProjectTest>();
 			builder.Services.AddScoped<IRepositoryProjectResource, RepositoryProjectResource>();
 			builder.Services.AddScoped<IRepositoryProjectCommisionReport, RepositoryProjectCommisionReport>();
+			
 
 			// Add our services
 			builder.Services.AddScoped<IServiceAuthentication, ServiceAuthentication>();
